3d92ce2d216bf2ea0efa6bc51cbf3c3e363d9983
mingi1123 <min_gi1123@naver.com>
2023-03-27 16:36:54 +0900

    map view

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MapMain from './Map/MapMain'
+
+const Main = () => {
+    return (
+        // <h1> SSU POWER </h1>    
+        <MapMain />
+    );
+}
+
+export default Main;
