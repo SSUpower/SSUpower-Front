8329c218bef6c90c891151ee37cf84e4629f9e80
chasyuss <cotn963@gmail.com>
2023-03-29 15:43:47 +0900

    LoginPage

---
+++ src/components/Login/Loginpage.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+function LoginPage() {
+    return (
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height:'100vh'
+        }}> 
+        
+            <form style = {{display: 'flex', flexDirection: 'column'}}>
+                <label>Email</label>
+                <input type="email" value onChange />
+
+                <label>Password</label>
+                <input type="password" value onChange />
+
+                <br />
+                <button>
+                    로그인  
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default LoginPage
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import MapMain from './Map/MapMain'
-
+import LoginPage from './Login/Loginpage';
 const Main = () => {
     return (
         // <h1> SSU POWER </h1>    
-        <MapMain />
+        //<MapMain />
+        <LoginPage />
     );
 }
 
