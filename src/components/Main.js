7ac4f1e4e9a3b4667134fa8847d1ffde98cda7bf
sihyuuun <leesh3597@gmail.com>
2023-03-30 16:23:03 +0900

    Feat: Add timetable
    라이브러리 설치해가지구 확인 바랍니다요
    react-timetable-events 라이브러리입니다 참고용!

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import MapMain from './Map/MapMain'
-
+import React from "react";
+import MapMain from "./Map/MapMain";
+import TimetableMain from "./TimeTable/TimetableMain";
 const Main = () => {
-    return (
-        // <h1> SSU POWER </h1>    
-        <MapMain />
-    );
-}
+  return (
+    // <h1> SSU POWER </h1>
+    //<MapMain />
+    <TimetableMain />
+  );
+};
 
 export default Main;
