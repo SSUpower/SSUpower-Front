47ef22d7963068a1a001b78638689b83c72d9561
mingi1123 <min_gi1123@naver.com>
2023-04-03 18:26:24 +0900

    index margin : 0

--- src/components/Map/MapMain.js
+++ src/components/Map/MapMain.js
@@ -24,13 +24,10 @@ const MapMain = () => {
 export default MapMain;
 
 const Wrapper = styled.div`
+	position: relative; // 추가
 	display: flex;
 	align-items: center;
 	text-align: center;
-	// flex-flow: column wrap;
-	// padding-top: 40px;
-	// padding-left: 30px;
-	// padding-right: 30px;
-	// padding-bottom: 20px;
-	// border-bottom: 2px solid #e9ecef;
+	width: 100%;
+	height: 100vh;
 `;
