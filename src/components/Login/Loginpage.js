b41998ba6df98d02f7bf3998262f37e5b2bc48af
chasyuss <cotn963@gmail.com>
2023-04-03 16:49:12 +0900

    login page에서 email이랑 password 빈칸으로 나오게 했고, 이메일 비번 치면 로그에 정보 저장되게 해서 업로드함당!

--- src/components/Login/Loginpage.js
+++ src/components/Login/Loginpage.js
@@ -1,18 +1,41 @@
-import React from "react"
+import React, { useState } from "react"
 
 function LoginPage() {
+    //이메일, 비밀번호 칸 비워두기
+    const [Email, setEmail] = useState("") //eslint-disable-line no-unused-vars
+    const [Password, setPassword] = useState("") //eslint-disable-line no-unused-vars
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        //사용자의 이메일, 비밀번호를 저장 
+        console.log ('Email',Email)
+        console.log ('Password',Password)
+
+    }
     return (
-        <div style={{
+        <div style={{ // 정렬
             display: 'flex', justifyContent: 'center', alignItems: 'center',
             width: '100%', height:'100vh'
-        }}> 
+        }}>
         
-            <form style = {{display: 'flex', flexDirection: 'column'}}>
+            <form style = {{display: 'flex', flexDirection: 'column'}}
+                onSubmit={onSubmitHandler}
+            >
                 <label>Email</label>
-                <input type="email" value onChange />
+                <input type="email" value={Email} onChange={onEmailHandler} />
 
                 <label>Password</label>
-                <input type="password" value onChange />
+                <input type="password" value={Password} onChange={onPasswordHandler} />
 
                 <br />
                 <button>
