28e169cb950d2c070eb956633ef94c21aa67cbde
mingi1123 <min_gi1123@naver.com>
2023-04-03 17:51:55 +0900

    make marker array

--- src/components/Map/KakaoMapScript.js
+++ src/components/Map/KakaoMapScript.js
@@ -9,23 +9,43 @@ export default function KakaoMapScript() {
         level: 3 //확대 레벨
     };
 
+    const positions = [
+        {
+          title: '정보과학관',
+          latlng: new kakao.maps.LatLng(37.4944064, 126.9599747)
+        },
+        {
+          title: '전산관',
+          latlng: new kakao.maps.LatLng(37.495422, 126.959512)
+        },
+        {
+          title: '진리관',
+          latlng: new kakao.maps.LatLng(37.496895, 126.957446)
+        }
+      ];
+
     const map = new kakao.maps.Map(container, options); //지도 생성
-
-    let imageSize = new kakao.maps.Size(50, 50), // 마커이미지의 크기입니다
-    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-
-    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-    let markerImage = new kakao.maps.MarkerImage(MarkerY, imageSize, imageOption);
+    let imageSize = new kakao.maps.Size(50, 50); // 마커이미지의 크기입니다
+    const markerImage = new kakao.maps.MarkerImage(MarkerY, imageSize); //마커 이미지 생성
 
     //마커 좌표
-    let markerPosition = new kakao.maps.LatLng(37.4944064, 126.9599747);
+    // let markerPosition = new kakao.maps.LatLng(37.4944064, 126.9599747);
   
-    //마커 생성
-    let marker = new kakao.maps.Marker({
-        position: markerPosition,
-        image: markerImage // 마커이미지 설정 
-    });
-  
-    //마커 표시
-    marker.setMap(map);
+    // //마커 생성
+    // let marker = new kakao.maps.Marker({
+    //     position: markerPosition,
+    //     image: markerImage // 마커이미지 설정 
+    // });
+
+    positions.forEach(position => {
+        const marker = new kakao.maps.Marker({
+          map: map,
+          position: position.latlng,
+          title: position.title,
+          image: markerImage
+        });
+        //마커 표시
+        marker.setMap(map);
+      });
+    
 }
