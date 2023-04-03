8644cdcfb65eb305b7aaca9483d69e9b67ad35a2
mingi1123 <min_gi1123@naver.com>
2023-04-03 17:22:59 +0900

    merge all

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/Login/Loginpage';
 import Main from './components/Main';
+import TimetableMain from './components/TimeTable/TimetableMain';
+import LoginPage from './components/Login/Loginpage';
 
 function App() {
   return (
     <Router>
             <Routes>
                 <Route path="/" element={<Main />} />
-                <Route exact path="/login" component={LoginPage} />
-        
+                <Route path="/login" element={ <LoginPage />} />
+                <Route path="/timetable" element={ <TimetableMain />} />
             </Routes>
         </Router>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import MapMain from './Map/MapMain'
-import LoginPage from './Login/Loginpage';
+
 const Main = () => {
     return (
         // <h1> SSU POWER </h1>    
-        //<MapMain />
-        <LoginPage />
+        <MapMain />
     );
 }
 
