2389cc6b1d66b515a58e0a87a6da57cb9537d3d2
chasyuss <cotn963@gmail.com>
2023-04-10 19:02:52 +0900

    decoration and add join page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './components/Main';
 import TimetableMain from './components/TimeTable/TimetableMain';
 import LoginPage from './components/Login/Loginpage';
+import Join from './components/Login/Join';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
                 <Route path="/" element={<Main />} />
                 <Route path="/login" element={ <LoginPage />} />
                 <Route path="/timetable" element={ <TimetableMain />} />
+                <Route path="/join" element={ <Join />} />
             </Routes>
         </Router>
   );
