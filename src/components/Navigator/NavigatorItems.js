e22fd1594a6e26897ba4eb7a47fa6a6798456e08
mingi1123 <min_gi1123@naver.com>
2023-06-03 20:31:24 +0900

    feat: Add navigation using useNavigate hook

--- src/components/Navigator/NavigatorItems.js
+++ src/components/Navigator/NavigatorItems.js
@@ -1,18 +1,42 @@
 import React, {useState, useEffect} from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const NavigatorItems = ({isMobile}) => {
     const [items, setItems] = useState([]);
+    const navigate = useNavigate();
 
 	useEffect(() => {
 		if (isMobile) setItems(["지도", "시간표"]);
 		else setItems(["지도", "시간표", "Login", "Join"]);
 	}, [isMobile])
 
+  const handleItemClick = (item) => {
+    // 원하는 링크로 이동하는 로직을 작성합니다.
+    switch (item) {
+      case "지도":
+        navigate("/map");
+        break;
+      case "시간표":
+        navigate("/schedule");
+        break;
+      case "Login":
+        navigate("/login");
+        break;
+      case "Join":
+        navigate("/join");
+        break;
+      default:
+        break;
+    }
+  };
+
 	return (
 		<NavItems>
 			{items.map((item) => (
-				<NavItem key={item}>{item}</NavItem>
+				<NavItem key={item} onClick={() => handleItemClick(item)}>
+        {item}
+      </NavItem>
 			))}
 		</NavItems>
 	);
